25f850263942ea1ccb149cb3beeaff34168ebba3
Guilherme <ggn.noronha@gmail.com>
2020-03-21 23:44:53 -0300

    Login page layout initial

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,13 +1,133 @@
 import React from "react";
-import { View } from "react-native";
-import { TouchButton, ButtonText, ViewContainer } from "./styles";
+import { View, StyleSheet } from "react-native";
+import {
+  ViewContainer,
+  HeaderContainer,
+  LogoImage,
+  ContentContainer,
+  CardBase,
+  RowCard,
+  CircleAccount,
+  CircleAccountTxt,
+  AccountDetail,
+  NameAccount,
+  NumberAccount,
+  BtnChangeAccount,
+  BtnChangeAccountText,
+  BtnSignIn,
+  BtnSignInText
+} from "./styles";
+import BottomSheet from "reanimated-bottom-sheet";
 
-export default function Login() {
+export default function Login({ navigation }) {
+  const renderHeader = () => (
+    <View style={styles.header}>
+      <View style={styles.panelHeader}>
+        <View style={styles.panelHandle} />
+      </View>
+    </View>
+  );
+  renderContent = () => <View style={styles.panel}></View>;
+  bs = React.createRef();
   return (
     <ViewContainer>
-      <TouchButton>
-        <ButtonText>Login page</ButtonText>
-      </TouchButton>
+      <HeaderContainer>
+        <LogoImage source={require("../../../assets/bancointer2.png")} />
+      </HeaderContainer>
+      <ContentContainer>
+        <CardBase>
+          <RowCard>
+            <CircleAccount>
+              <CircleAccountTxt>GG</CircleAccountTxt>
+            </CircleAccount>
+            <AccountDetail>
+              <NameAccount>GUILHERME GOMES NORO...</NameAccount>
+              <NumberAccount>983702-7</NumberAccount>
+            </AccountDetail>
+            <BtnChangeAccount>
+              <BtnChangeAccountText>ALTERAR</BtnChangeAccountText>
+            </BtnChangeAccount>
+          </RowCard>
+          <BtnSignIn>
+            <BtnSignInText>ENTRAR</BtnSignInText>
+          </BtnSignIn>
+        </CardBase>
+      </ContentContainer>
+
+      {/* <BottomSheet
+        ref={bs}
+        snapPoints={[450, 300, 0]}
+        renderContent={renderContent}
+        renderHeader={renderHeader}
+      /> */}
     </ViewContainer>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#F5FCFF"
+  },
+
+  panelContainer: {
+    position: "absolute",
+    top: 0,
+    bottom: 0,
+    left: 0,
+    right: 0
+  },
+  panel: {
+    height: 600,
+    padding: 20,
+    backgroundColor: "#f7f5eee8"
+  },
+  header: {
+    backgroundColor: "#f7f5eee8",
+    shadowColor: "#000000",
+    paddingTop: 20,
+    borderTopLeftRadius: 20,
+    borderTopRightRadius: 20
+  },
+  panelHeader: {
+    alignItems: "center"
+  },
+  panelHandle: {
+    width: 40,
+    height: 8,
+    borderRadius: 4,
+    backgroundColor: "#00000040",
+    marginBottom: 10
+  },
+  panelTitle: {
+    fontSize: 27,
+    height: 35
+  },
+  panelSubtitle: {
+    fontSize: 14,
+    color: "gray",
+    height: 30,
+    marginBottom: 10
+  },
+  panelButton: {
+    padding: 20,
+    borderRadius: 10,
+    backgroundColor: "#318bfb",
+    alignItems: "center",
+    marginVertical: 10
+  },
+  panelButtonTitle: {
+    fontSize: 17,
+    fontWeight: "bold",
+    color: "white"
+  },
+  photo: {
+    width: "100%",
+    height: 225,
+    marginTop: 30
+  },
+  map: {
+    height: "100%",
+    width: "100%"
+  }
+});
--- src/pages/login/styles.js
+++ src/pages/login/styles.js
@@ -1,23 +1,113 @@
 import styled from "styled-components/native";
+import { Dimensions } from "react-native";
+const { width } = Dimensions.get("window");
 
-const TouchButton = styled.TouchableHighlight`
-    width: 100px;
-    height: 40px;
-    padding: 12px;
-    border-radius: 10px;	
-    background-color: #FFa500;
-`;
-const ButtonText = styled.Text`
-  font-size: 15px;
-  color: #fff;
-  text-align: center;
-`;
+const AZULCINZA = "#ECEFF1";
 
-const ViewContainer = styled.View`
+const ViewContainer = styled.SafeAreaView`
   flex: 1;
-  background-color: #fff;
   align-items: center;
   justify-content: center;
 `;
+const HeaderContainer = styled.View`
+  background: #f96502;
+  justify-content: center;
+  height: 185px;
 
-export { TouchButton, ButtonText, ViewContainer };
+  /* width: 100%; */
+`;
+const LogoImage = styled.Image`
+  height: 44px;
+  resize-mode: center;
+`;
+const ContentContainer = styled.View`
+  flex: 1;
+  margin-horizontal: 20px;
+  width: ${width - 40};
+`;
+const CardBase = styled.View`
+  background: ${AZULCINZA};
+  justify-content: center;
+  align-items: center;
+  margin-top: -20px;
+  height: 200px;
+  border-radius: 10px;
+`;
+const RowCard = styled.View`
+  width: 100%;
+  justify-content: space-around;
+  flex-direction: row;
+  padding-horizontal: 20px;
+`;
+const CircleAccountTxt = styled.Text`
+  color: #fff;
+  font-size:15px
+  font-weight: 600;
+`;
+const CircleAccount = styled.View`
+  width: 50px;
+  height: 50px;
+  background: #e6e7f0;
+  border-radius: 25px;
+
+  align-items: center;
+  justify-content: center;
+`;
+const AccountDetail = styled.View``;
+const NameAccount = styled.Text`
+  max-width: 100px;
+`;
+const NumberAccount = styled.Text``;
+const BtnChangeAccount = styled.TouchableOpacity`
+  width: 100px;
+  height: 40px;
+  padding: 12px;
+  align-items: center;
+  justify-content: center;
+
+  background-color: transparent;
+  border: #fff 1px;
+  border-color: #fff;
+  border-radius: 5px;
+`;
+const BtnChangeAccountText = styled.Text`
+  font-size: 15px;
+  color: #000;
+  font-weight: bold;
+  text-align: center;
+`;
+const BtnSignIn = styled.TouchableOpacity`
+  align-items: center;
+  justify-content: center;
+
+  margin-top: 25px;
+  width: 220px;
+  height: 50px;
+
+  border-radius: 5px;
+  background-color: #f96502;
+`;
+const BtnSignInText = styled.Text`
+  font-size: 15px;
+  color: #fff;
+  text-align: center;
+  font-weight: bold;
+`;
+
+export {
+  ViewContainer,
+  HeaderContainer,
+  LogoImage,
+  ContentContainer,
+  CardBase,
+  RowCard,
+  CircleAccount,
+  CircleAccountTxt,
+  AccountDetail,
+  NameAccount,
+  NumberAccount,
+  BtnChangeAccount,
+  BtnChangeAccountText,
+  BtnSignIn,
+  BtnSignInText
+};
